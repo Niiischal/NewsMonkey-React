6adbd1db7a7649398701d9c8ecd3b948b9e97555
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-07 22:22:31 +0545

    adding structure for the news items

--- newsmonkey/src/components/News.js
+++ newsmonkey/src/components/News.js
@@ -4,13 +4,19 @@ import NewsItems from './NewsItems'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a component.
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
+      <div className='container my-3'>
+        <h2>NewsMonkey- Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+          <NewsItems title="myTitle" description="myDesc"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItems title="myTitle" description="myDesc"/>
+          </div>
+          <div className="col-md-4">
+          <NewsItems title="myTitle" description="myDesc"/>
+          </div>
+        </div>
       </div>
     )
   }
--- newsmonkey/src/components/NewsItems.js
+++ newsmonkey/src/components/NewsItems.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class NewsItems extends Component {
   render() {
+    let {title , description} = this.props;
     return (
       <div>
-        This is a news item.
+        <div className="card" style={{width: "18rem"}}>
+          <img src="https://apnews.com/article/russia-ukraine-war-kakhovka-dam-flood-evacuation-eecc9952c2d9f500c38b0a873f69438c" className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href="/" className="btn btn-primary">Go somewhere</a>
+            </div>
+       </div>
       </div>
     )
   }
