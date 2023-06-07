e2f3490fcdc7eec09190f5616cdf40304529f2ad
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-07 22:48:09 +0545

    using state and constructor

--- newsmonkey/src/components/NewsItems.js
+++ newsmonkey/src/components/NewsItems.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
 export class NewsItems extends Component {
   render() {
-    let {title , description} = this.props;
+    let {title , description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://apnews.com/article/russia-ukraine-war-kakhovka-dam-flood-evacuation-eecc9952c2d9f500c38b0a873f69438c" className="card-img-top" alt="..."/>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href="/" className="btn btn-primary">Go somewhere</a>
+              <a href="/" className="btn btn-sm btn-primary">Read More</a>
             </div>
        </div>
       </div>
