88af8c612904a9d316fdf5b8cc9dcd15da0b42a6
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-08 19:01:59 +0545

    using fetch api

--- newsmonkey/src/components/News.js
+++ newsmonkey/src/components/News.js
@@ -272,6 +272,13 @@ export class News extends Component {
     }
 
   }
+
+  async componentDidMount(){
+    let url= "https://newsapi.org/v2/top-headlines?country=us&apiKey=90c71cea048a4c9f9714415dd37eb9cc"
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({articles: parsedData.articles})
+  }
   render() {
     return (
       <div className='container my-3'>
@@ -279,7 +286,7 @@ export class News extends Component {
         <div className="row">
         {this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
-                    <NewsItems title={element.title.slice(0,63)} description={element.description?.slice(0,78)}  imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    <NewsItems title={element.title?.slice(0,63)} description={element.description?.slice(0,78)}  imageUrl={element.urlToImage} newsUrl={element.url}/>
                     </div>
         })}
 
