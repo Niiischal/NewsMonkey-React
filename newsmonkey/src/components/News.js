42ee0031cdd563916b668fb621da12f7eeb971cd
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-08 19:46:13 +0545

    adding previous and next buttons with logic

--- newsmonkey/src/components/News.js
+++ newsmonkey/src/components/News.js
@@ -6,17 +6,46 @@ export class News extends Component {
     super()
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1,
+
     }
 
   }
 
   async componentDidMount(){
-    let url= "https://newsapi.org/v2/top-headlines?country=us&apiKey=90c71cea048a4c9f9714415dd37eb9cc"
+    let url= "https://newsapi.org/v2/top-headlines?country=us&apiKey=90c71cea048a4c9f9714415dd37eb9cc&pageSize=18"
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
   }
+  handlePreviousClick = async ()=>{
+    let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=90c71cea048a4c9f9714415dd37eb9cc&page=${this.state.page - 1}&pageSize=18`
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState(
+      {
+        page: this.state.page - 1,
+        articles: parsedData.articles
+      }
+    )  
+  }
+  handleNextClick = async()=>{
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+    let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=90c71cea048a4c9f9714415dd37eb9cc&page=${this.state.page + 1}&pageSize=18`
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState(
+      {
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      }
+    )
+  }
+}
   render() {
     return (
       <div className='container my-3'>
@@ -25,9 +54,12 @@ export class News extends Component {
         {this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
                     <NewsItems title={element.title?.slice(0,63)} description={element.description?.slice(0,78)}  imageUrl={element.urlToImage} newsUrl={element.url}/>
-                    </div>
+            </div>
         })}
-
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&laquo; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &raquo;</button>
         </div>
       </div>
     )
