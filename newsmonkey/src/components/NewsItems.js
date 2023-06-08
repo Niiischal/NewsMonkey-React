f8fd5b524884bf96ce35c0aab9c3a68f020d1c6b
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-08 19:12:10 +0545

    adding only the api key, removing previous articles

--- newsmonkey/src/components/NewsItems.js
+++ newsmonkey/src/components/NewsItems.js
@@ -6,7 +6,7 @@ export class NewsItems extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={!imageUrl?"https://i.ytimg.com/vi/Jvpt-3VaIH0/maxresdefault.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
