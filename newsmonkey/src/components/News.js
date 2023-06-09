9ea3a5d7b2222c116c65a40739b35df1395cc6f5
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-09 21:36:42 +0545

    adding author and time published

--- newsmonkey/src/components/News.js
+++ newsmonkey/src/components/News.js
@@ -62,7 +62,7 @@ export class News extends Component {
         <div className="row">
         {!this.state.loading && this.state.articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
-                    <NewsItems title={element.title} description={element.description}  imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    <NewsItems title={element.title} description={element.description}  imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} />
             </div>
         })}
         </div>
--- newsmonkey/src/components/NewsItems.js
+++ newsmonkey/src/components/NewsItems.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export class NewsItems extends Component {
   render() {
-    let {title , description, imageUrl, newsUrl} = this.props;
+    let {title , description, imageUrl, newsUrl, author, publishedAt } = this.props;
     return (
       <div className='my-3'>
         <div className="card">
@@ -10,7 +10,8 @@ export class NewsItems extends Component {
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+              <p className="card-text"><small className="text-body-secondary">By: {!author?"Unknown" :author} on {publishedAt}</small></p>
+              <a href={newsUrl} className="btn btn-sm btn-dark">Read More</a>
             </div>
        </div>
       </div>
